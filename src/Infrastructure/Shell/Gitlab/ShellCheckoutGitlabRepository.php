797828c559b891c60817c77f869c1ed5a886cb13
Jan Mikeš <j.mikes@me.com>
2021-02-14 00:30:31 +0100

    Implemented shell Gitlab Checkout repository

--- src/Infrastructure/Shell/Gitlab/ShellCheckoutGitlabRepository.php
+++ src/Infrastructure/Shell/Gitlab/ShellCheckoutGitlabRepository.php
@@ -5,7 +5,6 @@ namespace Acme\Infrastructure\Shell\Gitlab;
 
 use Acme\Domain\Gitlab\CheckoutGitlabRepository;
 use Acme\Domain\Gitlab\GitlabApplication;
-use Nette\Utils\FileSystem;
 
 final class ShellCheckoutGitlabRepository implements CheckoutGitlabRepository
 {
@@ -21,8 +20,47 @@ final class ShellCheckoutGitlabRepository implements CheckoutGitlabRepository
     public function __invoke(string $repositoryName): GitlabApplication
     {
         $repositoryDirectory = $this->repositoriesCommonDirectory . '/' . $repositoryName;
-        FileSystem::createDir($repositoryDirectory);
+
+        // TODO: clone private repositories
+        // TODO: clone self-hosted gitlab repositories (non gitlab.com domain)
+
+        $repositoryUrl = 'https://gitlab.com/'. $repositoryName . '.git';
+
+        if ($this->isRepositoryAlreadyCloned($repositoryDirectory)) {
+            $this->updateRepository($repositoryDirectory);
+        } else {
+            $this->cloneRepository($repositoryDirectory, $repositoryUrl);
+        }
 
         return new GitlabApplication();
+    }
+
+
+    private function cloneRepository(string $repositoryDirectory, string $repositoryUrl): void
+    {
+        $command = sprintf(
+            'git clone %s %s',
+            $repositoryUrl,
+            $repositoryDirectory,
+        );
+
+        echo shell_exec($command);
+    }
+
+
+    private function isRepositoryAlreadyCloned(string $repositoryDirectory): bool
+    {
+        return is_dir($repositoryDirectory);
+    }
+
+
+    private function updateRepository(string $repositoryDirectory): void
+    {
+        $command = sprintf(
+            'cd %s && git fetch && git pull --rebase',
+            $repositoryDirectory
+        );
+
+        echo shell_exec($command);
     }
 }
