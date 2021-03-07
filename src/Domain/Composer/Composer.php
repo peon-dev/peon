d04e84d0685377fe78d792f087717e0d6829f3b3
Jan Mikeš <j.mikes@me.com>
2021-03-07 23:17:35 +0100

    Reversed dependencies - dropped Application object

--- src/Domain/Composer/Composer.php
+++ src/Domain/Composer/Composer.php
@@ -9,5 +9,5 @@ interface Composer
     /**
      * @throws ComposerJsonFileMissing
      */
-    public function install(string $directory): void;
+    public function installInDirectory(string $directory): void;
 }
--- src/UseCase/RunRectorOnGitlabRepositoryOpenCreateMergeRequestUseCase.php
+++ src/UseCase/RunRectorOnGitlabRepositoryOpenCreateMergeRequestUseCase.php
@@ -3,18 +3,16 @@ declare (strict_types=1);
 
 namespace PHPMate\UseCase;
 
-use PHPMate\Domain\Application\Application;
 use PHPMate\Domain\Composer\Composer;
 use PHPMate\Domain\Git\Git;
-use PHPMate\Domain\Gitlab\GitlabApi;
-use PHPMate\Domain\Gitlab\GitlabRepository;
+use PHPMate\Domain\Gitlab\Gitlab;
 use PHPMate\Domain\Rector\Rector;
 
 final class RunRectorOnGitlabRepositoryOpenCreateMergeRequestUseCase
 {
     public function __construct(
         private Git $git,
-        private GitlabApi $gitlabApi,
+        private Gitlab $gitlabApi,
         private Composer $composer,
         private Rector $rector
     ) {}
@@ -22,16 +20,14 @@ final class RunRectorOnGitlabRepositoryOpenCreateMergeRequestUseCase
 
     public function __invoke(string $repositoryUri, string $username, string $personalAccessToken): void
     {
-        $directory = ''; // TODO: Some service should provide this
+        $directory = __DIR__ . '/../../var'; // TODO: Some service should provide this
         $repositoryName = ''; // TODO: Some service should extract repository name from remoteUri
         $remoteUri = ''; // TODO: some magic, add $username + $accesstoken into $remoteUri
 
         $this->git->clone($directory, $remoteUri);
 
-        $application = Application::fromDirectory($directory);
-
-        $application->installComposer($this->composer);
-        $application->runRector($this->rector);
+        $this->composer->installInDirectory($directory);
+        $this->rector->runInDirectory($directory);
 
         if ($this->git->hasUncommittedChanges($directory)) {
             $branchWithChanges = 'improvements';
@@ -39,8 +35,7 @@ final class RunRectorOnGitlabRepositoryOpenCreateMergeRequestUseCase
             $this->git->checkoutNewBranch($directory, $branchWithChanges);
             $this->git->commitChanges($directory, 'Changes by PHP Mate');
 
-            $gitlabRepository = GitlabRepository::fromPersonalAccessToken($repositoryName, $personalAccessToken);
-            $gitlabRepository->openMergeRequest($branchWithChanges, $this->gitlabApi);
+            $this->gitlabApi->openMergeRequest($repositoryName, $personalAccessToken, $branchWithChanges);
         }
     }
 }
