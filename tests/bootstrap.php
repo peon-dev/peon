e3c2691eb8a9e9f0822f87dc5f4ec3194eb61576
Jan Mikeš <j.mikes@me.com>
2021-04-04 12:13:35 +0200

    Added tracy + keeping log dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use Tracy\Debugger;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = new Symfony\Component\Dotenv\Dotenv();
 $dotenv->loadEnv(__DIR__ . '/../.env', null, 'test');
+
+Debugger::enable(logDirectory: __DIR__ . '/../var/log');
