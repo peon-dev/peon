f1f64e27d4e75befedce000e2a714ebda0b83c66
Jan Mikeš <j.mikes@me.com>
2021-04-07 22:11:44 +0200

    Fix Rector tests to not use filesystem

--- tests/Domain/Rector/RectorTest.php
+++ tests/Domain/Rector/RectorTest.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace PHPMate\Tests\Domain\Rector;
 
-use League\Flysystem\FilesystemReader;
+use PHPMate\Domain\FileSystem\WorkingDirectory;
 use PHPMate\Domain\Rector\Rector;
 use PHPMate\Domain\Rector\RectorBinary;
 use PHPMate\Domain\Rector\RectorConfigFileMissing;
@@ -13,8 +13,7 @@ class RectorTest extends TestCase
 {
     public function testRunInWorkingDirectory(): void
     {
-        $workingDirectory = '.';
-        $filesystemReader = $this->getFilesystemReader(true);
+        $workingDirectory = $this->getWorkingDirectory(true);
 
         $rectorBinary = $this->createMock(RectorBinary::class);
         $rectorBinary->expects(self::once())
@@ -24,7 +23,7 @@ class RectorTest extends TestCase
                 'process'
             );
 
-        $rector = new Rector($rectorBinary, $filesystemReader);
+        $rector = new Rector($rectorBinary);
         $rector->runInWorkingDirectory($workingDirectory);
     }
 
@@ -34,20 +33,20 @@ class RectorTest extends TestCase
         $this->expectException(RectorConfigFileMissing::class);
 
         $rectorBinary = $this->createStub(RectorBinary::class);
-        $filesystemReader = $this->getFilesystemReader(false);
+        $workingDirectory = $this->getWorkingDirectory(false);
 
-        $rector = new Rector($rectorBinary, $filesystemReader);
-        $rector->runInWorkingDirectory('.');
+        $rector = new Rector($rectorBinary);
+        $rector->runInWorkingDirectory($workingDirectory);
     }
 
 
-    private function getFilesystemReader(bool $rectorConfigFileExists): FilesystemReader
+    private function getWorkingDirectory(bool $rectorConfigFileExists): WorkingDirectory
     {
-        $filesystemReader = $this->createMock(FilesystemReader::class);
-        $filesystemReader->method('fileExists')
-            ->with('./rector.php')
+        $workingDirectory = $this->createMock(WorkingDirectory::class);
+        $workingDirectory->method('fileExists')
+            ->with('rector.php')
             ->willReturn($rectorConfigFileExists);
 
-        return $filesystemReader;
+        return $workingDirectory;
     }
 }
