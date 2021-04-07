8bddd7136d84a03e3ad43750bd067bc16708a086
Jan Mikeš <j.mikes@me.com>
2021-04-07 23:34:48 +0200

    Preparations for cleanup and MR existence assertions

--- tests/UseCase/RunRectorOnGitlabRepositoryUseCaseTest.php
+++ tests/UseCase/RunRectorOnGitlabRepositoryUseCaseTest.php
@@ -3,19 +3,19 @@ declare(strict_types=1);
 
 namespace PHPMate\Tests\UseCase;
 
+use _HumbugBox5ccdb2ccdb35\Nette\DI\ContainerBuilder;
+use PHPMate\Domain\Git\BranchNameProvider;
 use PHPMate\Infrastructure\Symfony\DependencyInjection\ContainerFactory;
 use PHPMate\UseCase\RunRectorOnGitlabRepositoryUseCase;
 use PHPUnit\Framework\TestCase;
 
 class RunRectorOnGitlabRepositoryUseCaseTest extends TestCase
 {
-    // TODO: we need to verify MR is created, how?
-    // TODO: test should clean after itself, how? (delete branch + MR?)
     public function test(): void
     {
         $container = ContainerFactory::create();
-        $useCase = $container->get(RunRectorOnGitlabRepositoryUseCase::class);
 
+        $useCase = $container->get(RunRectorOnGitlabRepositoryUseCase::class);
         self::assertInstanceOf(RunRectorOnGitlabRepositoryUseCase::class, $useCase);
 
         // Populate values in `.env.test.local`
@@ -24,5 +24,24 @@ class RunRectorOnGitlabRepositoryUseCaseTest extends TestCase
         $personalAccessToken = $_SERVER['TEST_GITLAB_PERSONAL_ACCESS_TOKEN'];
 
         $useCase->__invoke($repositoryUri, $username, $personalAccessToken);
+
+        $branchNameProvider = $container->get(BranchNameProvider::class);
+        self::assertInstanceOf(BranchNameProvider::class, $branchNameProvider);
+        $branchName = $branchNameProvider->provideForProcedure('rector');
+
+        $this->assertMergeRequestExists($branchName);
+        $this->removeBranch($branchName);
+    }
+
+
+    private function assertMergeRequestExists(string $branchName): void
+    {
+        // TODO
+    }
+
+
+    private function removeBranch(string $branchName): void
+    {
+        // TODO
     }
 }
