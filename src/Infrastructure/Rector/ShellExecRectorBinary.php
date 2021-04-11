2d647d6c667f6af881c4dc275d5367c414e5960a
Jan Mikeš <j.mikes@me.com>
2021-04-11 10:25:48 +0200

    Removing WorkingDirectory from Rector

--- src/Infrastructure/Rector/ShellExecRectorBinary.php
+++ src/Infrastructure/Rector/ShellExecRectorBinary.php
@@ -4,18 +4,17 @@ declare(strict_types=1);
 
 namespace PHPMate\Infrastructure\Rector;
 
-use PHPMate\Domain\FileSystem\WorkingDirectory;
 use PHPMate\Domain\Rector\RectorBinary;
 
 final class ShellExecRectorBinary implements RectorBinary
 {
     private const BINARY_EXECUTABLE = '../../vendor/bin/rector'; // TODO must be in root
 
-    public function executeCommand(WorkingDirectory $projectDirectory, string $command): void
+    public function executeCommand(string $directory, string $command): void
     {
         $command = sprintf(
             'cd %s && %s %s',
-            $projectDirectory->getAbsolutePath(),
+            $directory,
             self::BINARY_EXECUTABLE,
             $command
         );
