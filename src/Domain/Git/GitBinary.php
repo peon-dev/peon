e8cf952a80e6a5cf95aa31eb2591513fea318758
Jan Mikeš <j.mikes@me.com>
2021-04-11 10:25:17 +0200

    Removing WorkingDirectory from Git

--- src/Domain/Git/GitBinary.php
+++ src/Domain/Git/GitBinary.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace PHPMate\Domain\Git;
 
-use PHPMate\Domain\FileSystem\WorkingDirectory;
-
 interface GitBinary
 {
-    public function executeCommand(WorkingDirectory $projectDirectory, string $command): string;
+    public function executeCommand(string $directory, string $command): string;
 }
