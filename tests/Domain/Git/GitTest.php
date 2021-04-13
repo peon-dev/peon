2bf6316bbe6d05cf7db8b04bd04545ceeaf0eb99
Jan Mikeš <j.mikes@me.com>
2021-04-13 18:30:36 +0200

    Empty git test

---
+++ tests/Domain/Git/GitTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPMate\Tests\Domain\Git;
+
+use PHPMate\Domain\Git\Git;
+use PHPUnit\Framework\TestCase;
+
+class GitTest extends TestCase
+{
+    public function testCommitAndPushChanges(): void
+    {
+
+    }
+
+
+    public function testGetCurrentBranch(): void
+    {
+
+    }
+
+
+    public function testHasUncommittedChanges(): void
+    {
+
+    }
+
+
+    public function testClone(): void
+    {
+
+    }
+
+
+    public function testCheckoutNewBranch(): void
+    {
+
+    }
+}
