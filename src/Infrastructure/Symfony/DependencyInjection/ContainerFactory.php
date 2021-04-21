754c44ae1e1999f2a901644b0affff226f39b5a4
Jan Mikeš <j.mikes@me.com>
2021-04-21 23:59:32 +0200

    Added support for providing custom notifier

--- src/Infrastructure/Symfony/DependencyInjection/ContainerFactory.php
+++ src/Infrastructure/Symfony/DependencyInjection/ContainerFactory.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace PHPMate\Infrastructure\Symfony\DependencyInjection;
 
+use PHPMate\Domain\Notification\Notifier;
+use PHPMate\Infrastructure\Notifier\SymfonyNotifier;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\Notifier\NotifierInterface;
 
 class ContainerFactory
 {
+    public static ?NotifierInterface $symfonyNotifier = null;
+
     /**
      * @param array<string>
      */
@@ -22,6 +27,11 @@ class ContainerFactory
         $envSpecificConfig = 'config_' . $_ENV['APP_ENV'] . '.php';
         if (is_file(__DIR__ . '/' . $envSpecificConfig)) {
             $loader->load($envSpecificConfig);
+        }
+
+        if (self::$symfonyNotifier !== null) {
+            $containerBuilder->set(SymfonyNotifier::class, new SymfonyNotifier(self::$symfonyNotifier));
+            $containerBuilder->setAlias(Notifier::class, SymfonyNotifier::class);
         }
 
         foreach ($customConfigs as $customConfig) {
