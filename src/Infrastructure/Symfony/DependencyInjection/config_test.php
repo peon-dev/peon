186762022218e92d1804f123980625f0a16dc485
Jan Mikeš <j.mikes@me.com>
2021-05-05 00:00:40 +0200

    Added failing test for launcher + inmemory job repository

--- src/Infrastructure/Symfony/DependencyInjection/config_test.php
+++ src/Infrastructure/Symfony/DependencyInjection/config_test.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use PHPMate\Domain\Git\BranchNameProvider;
+use PHPMate\Domain\Job\JobRepository;
 use PHPMate\Infrastructure\Git\StatefulRandomPostfixBranchNameProvider;
+use PHPMate\Infrastructure\Memory\InMemoryJobRepository;
 
 return static function(ContainerConfigurator $configurator): void
 {
@@ -19,4 +21,7 @@ return static function(ContainerConfigurator $configurator): void
 
     $services->set(StatefulRandomPostfixBranchNameProvider::class);
     $services->alias(BranchNameProvider::class, StatefulRandomPostfixBranchNameProvider::class);
+
+    $services->set(InMemoryJobRepository::class);
+    $services->alias(JobRepository::class, InMemoryJobRepository::class);
 };
