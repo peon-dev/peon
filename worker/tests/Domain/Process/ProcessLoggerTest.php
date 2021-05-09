105a4fd461ec7bd57b93bf267913991cb06f0467
Jan Mikeš <j.mikes@me.com>
2021-05-09 11:03:39 +0200

    Add failing test for sanitizing logger output

---
+++ worker/tests/Domain/Process/ProcessLoggerTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPMate\Worker\Tests\Domain\Process;
+
+use PHPMate\Worker\Domain\Process\ProcessLogger;
+use PHPMate\Worker\Domain\Process\ProcessResult;
+use PHPUnit\Framework\TestCase;
+
+final class ProcessLoggerTest extends TestCase
+{
+    /**
+     * @dataProvider provideTestProcessOutputWillBeSanitizedData
+     */
+    public function testProcessOutputWillBeSanitized(string $processOutput, string $expectedSanitizedOutput): void
+    {
+        $processResult = new ProcessResult('', 0, $processOutput);
+
+        $logger = new ProcessLogger();
+        $logger->logResult($processResult);
+
+        $logs = $logger->getLogs();
+
+        $loggedResult = $logs[array_key_first($logs)];
+
+        self::assertSame($expectedSanitizedOutput, $loggedResult->output);
+    }
+
+
+    /**
+     * @return \Generator<array{string, string}>
+     */
+    public function provideTestProcessOutputWillBeSanitizedData(): \Generator
+    {
+        yield [
+            'git clone https://username:password-password@gitlab.com/phpmate/phpmate.git .',
+            'git clone https://username:****@gitlab.com/phpmate/phpmate.git .',
+        ];
+
+        yield [
+            'git clone https://username@gitlab.com/phpmate/phpmate.git .',
+            'git clone https://username@gitlab.com/phpmate/phpmate.git .',
+        ];
+    }
+}
