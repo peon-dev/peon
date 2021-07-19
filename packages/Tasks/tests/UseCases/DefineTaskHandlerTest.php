f2425fd95d806d0d1a4e349834b2d887607dfd7f
Jan Mikeš <j.mikes@me.com>
2021-07-20 00:46:59 +0200

    [Tasks] Added failing tests

--- packages/Tasks/tests/UseCases/DefineTaskHandlerTest.php
+++ packages/Tasks/tests/UseCases/DefineTaskHandlerTest.php
@@ -11,11 +11,15 @@ final class DefineTaskHandlerTest extends TestCase
 {
     public function testTaskCanBeDefined(): void
     {
+        // TODO: get + assert not exists
+
         $tasks = $this->createMock(Tasks::class);
         $tasks->expects(self::once())->method('provideNextIdentity');
         $tasks->expects(self::once())->method('save');
 
         $handler = new DefineTaskHandler();
         $handler->handle('Name', []);
+
+        // TODO: get + assert exists
     }
 }
--- packages/Tasks/tests/UseCases/RemoveTaskHandlerTest.php
+++ packages/Tasks/tests/UseCases/RemoveTaskHandlerTest.php
@@ -3,10 +3,38 @@ declare(strict_types=1);
 
 namespace PHPMate\Tasks\Tests\UseCases;
 
+use PHPMate\Tasks\TaskId;
+use PHPMate\Tasks\TaskNotFound;
+use PHPMate\Tasks\Tasks;
 use PHPMate\Tasks\UseCases\RemoveTaskHandler;
 use PHPUnit\Framework\TestCase;
 
 final class RemoveTaskHandlerTest extends TestCase
 {
+    public function testTaskCanBeRemoved(): void
+    {
+        // TODO: get + assert exists
 
+        $tasks = $this->createMock(Tasks::class);
+        $tasks->expects(self::once())->method('get');
+        $tasks->expects(self::once())->method('remove');
+
+        $handler = new RemoveTaskHandler();
+        $handler->handle(new TaskId(''));
+
+        // TODO: get + assert not exists
+    }
+
+
+    public function testNonExistingTaskCanNotBeRemoved(): void
+    {
+        $this->expectException(TaskNotFound::class);
+
+        $tasks = $this->createMock(Tasks::class);
+        $tasks->expects(self::once())->method('get')
+            ->willThrowException(new TaskNotFound());
+
+        $handler = new RemoveTaskHandler();
+        $handler->handle(new TaskId(''));
+    }
 }
