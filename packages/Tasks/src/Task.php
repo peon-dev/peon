3851383438dcdb86e88573a071e436ccb51d12e5
Jan Mikeš <j.mikes@me.com>
2021-07-20 00:51:50 +0200

    [Tasks] Task must not be empty without scripts

--- packages/Tasks/src/Task.php
+++ packages/Tasks/src/Task.php
@@ -14,7 +14,18 @@ final class Task
         private TaskId $taskId,
         private string $name,
         private array $scripts
-    ) {}
+    ) {
+        $this->checkThereAreSomeScripts($scripts);
+    }
 
-    // function update($name, $scripts)
+    /**
+     * @param array<string> $scripts
+     * @throws TaskCanNotHaveNoScripts
+     */
+    private function checkThereAreSomeScripts(array $scripts): void
+    {
+        if (count($scripts) <= 0) {
+            throw new TaskCanNotHaveNoScripts();
+        }
+    }
 }
