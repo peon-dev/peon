9e1f5f8a18d50f66b9638a80f74c6385a3aab786
Jan Mikeš <j.mikes@me.com>
2021-07-20 00:46:59 +0200

    [Tasks] Added failing tests

--- packages/Tasks/tests/UseCases/RedefineTaskHandlerTest.php
+++ packages/Tasks/tests/UseCases/RedefineTaskHandlerTest.php
@@ -3,10 +3,37 @@ declare(strict_types=1);
 
 namespace PHPMate\Tasks\Tests\UseCases;
 
+use PHPMate\Tasks\TaskId;
+use PHPMate\Tasks\TaskNotFound;
+use PHPMate\Tasks\Tasks;
+use PHPMate\Tasks\UseCases\DefineTaskHandler;
 use PHPMate\Tasks\UseCases\RedefineTaskHandler;
 use PHPUnit\Framework\TestCase;
 
 final class RedefineTaskHandlerTest extends TestCase
 {
+    public function testTaskCanBeRedefined(): void
+    {
+        $tasks = $this->createMock(Tasks::class);
+        $tasks->expects(self::once())->method('get');
+        $tasks->expects(self::once())->method('save');
 
+        $handler = new RedefineTaskHandler();
+        $handler->handle(new TaskId(''), 'Name', []);
+
+        // TODO: get + assert is changed
+    }
+
+
+    public function testNonExistingTaskCanNotBeRedefined(): void
+    {
+        $this->expectException(TaskNotFound::class);
+
+        $tasks = $this->createMock(Tasks::class);
+        $tasks->expects(self::once())->method('get')
+            ->willThrowException(new TaskNotFound());
+
+        $handler = new RedefineTaskHandler();
+        $handler->handle(new TaskId(''), 'Name', []);
+    }
 }
