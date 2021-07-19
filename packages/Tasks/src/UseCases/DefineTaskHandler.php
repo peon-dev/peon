7759bde2ddeb176febebac53dc128f568967b143
Jan Mikeš <j.mikes@me.com>
2021-07-20 00:57:27 +0200

    [Tasks] Implemented define task handler

--- packages/Tasks/src/UseCases/DefineTaskHandler.php
+++ packages/Tasks/src/UseCases/DefineTaskHandler.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 
 namespace PHPMate\Tasks\UseCases;
 
+use PHPMate\Tasks\Task;
+use PHPMate\Tasks\Tasks;
+
 final class DefineTaskHandler
 {
+    public function __construct(
+        private Tasks $tasks
+    ) {}
+
+
     /**
      * @param array<string> $scripts
      */
     public function handle(string $name, array $scripts): void
     {
+        $taskId = $this->tasks->provideNextIdentity();
+
+        $task = new Task($taskId, $name, $scripts);
+
+        $this->tasks->save($task);
     }
 }
