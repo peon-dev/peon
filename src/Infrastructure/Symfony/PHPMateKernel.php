5bf44e3219fc75e07af5d3e4ea96b02cb4c8ac5e
Jan Mikeš <j.mikes@me.com>
2021-08-19 16:40:05 +0200

    Tweak config paths

--- src/Infrastructure/Symfony/PHPMateKernel.php
+++ src/Infrastructure/Symfony/PHPMateKernel.php
@@ -13,26 +13,26 @@ class PHPMateKernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import(__DIR__ . '/config/{packages}/*.yaml');
+        $container->import(__DIR__ . '/config/{packages}/'.$this->environment.'/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
+            $container->import(__DIR__ . '/config/services.yaml');
+            $container->import(__DIR__ . '/config/{services}_'.$this->environment.'.yaml');
         } else {
-            $container->import('../config/{services}.php');
+            $container->import(__DIR__ . '/config/{services}.php');
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import(__DIR__ . '/config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import(__DIR__ . '/config/{routes}/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
+            $routes->import(__DIR__ . '/config/routes.yaml');
         } else {
-            $routes->import('../config/{routes}.php');
+            $routes->import(__DIR__ . '/config/{routes}.php');
         }
     }
 }
