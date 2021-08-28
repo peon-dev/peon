aff277282767c5075c95e2179bf497c2774baab7
Jan Mikeš <j.mikes@me.com>
2021-08-29 00:54:07 +0200

    Symfony configurations

---
+++ src/Infrastructure/Symfony/Config/services_test.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use PHPMate\Domain\Project\ProjectsCollection;
+use PHPMate\Domain\Task\TasksCollection;
+use PHPMate\Domain\Tools\Git\BranchNameProvider;
+use PHPMate\Domain\Job\JobsCollection;
+use PHPMate\Infrastructure\Git\StatefulRandomPostfixBranchNameProvider;
+use PHPMate\Infrastructure\Persistence\InMemory\InMemoryJobsCollection;
+use PHPMate\Infrastructure\Persistence\InMemory\InMemoryProjectsCollection;
+use PHPMate\Infrastructure\Persistence\InMemory\InMemoryTasksCollection;
+
+return static function(ContainerConfigurator $configurator): void
+{
+    $services = $configurator->services();
+
+    $services->defaults()
+        ->autoconfigure()
+        ->autowire()
+        ->public(); // Allow access services via container in tests
+
+    $services->set(BranchNameProvider::class, StatefulRandomPostfixBranchNameProvider::class);
+
+    $services->set(JobsCollection::class, InMemoryJobsCollection::class);
+
+    $services->set(TasksCollection::class, InMemoryTasksCollection::class);
+
+    $services->set(ProjectsCollection::class, InMemoryProjectsCollection::class);
+};
