373a208bc92da436cbc6753454de0f7bdb8dd82e
Jan Mikeš <j.mikes@me.com>
2021-08-29 14:31:38 +0200

    Fix phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Dotenv())->loadEnv(__DIR__ . '/../.env', defaultEnv: 'test');
