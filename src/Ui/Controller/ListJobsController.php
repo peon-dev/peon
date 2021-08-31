5b0367ccc524523c96ad7837fda81029071b2f18
Jan Mikeš <j.mikes@me.com>
2021-08-31 23:02:17 +0200

    Added button to add project + linking vars into template

--- src/Ui/Controller/ListJobsController.php
+++ src/Ui/Controller/ListJobsController.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace PHPMate\Ui\Controller;
 
+use PHPMate\Domain\Job\JobsCollection;
+use PHPMate\Domain\Project\ProjectsCollection;
+use PHPMate\Domain\Task\TasksCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class ListJobsController extends AbstractController
 {
+    public function __construct(
+        private JobsCollection $jobsCollection,
+        private ProjectsCollection $projectsCollection,
+        private TasksCollection $tasksCollection,
+    ) {}
+
+
     #[Route(path: '/', methods: ['GET'])]
     public function __invoke(): Response
     {
         return $this->render('jobs_list.html.twig', [
-            'jobs' => [],
+            'jobs' => $this->jobsCollection->getAll(),
+            'projects' => $this->projectsCollection->getAll(),
+            'tasks' => $this->tasksCollection->getAll(),
         ]);
     }
 }
