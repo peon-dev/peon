62762909ee5401bc6aca8b6a0d5280f2e51d6e4c
Jan Mikeš <j.mikes@me.com>
2021-09-01 11:14:01 +0200

    Added test for CreateProject usecase

---
+++ tests/Unit/UseCase/CreateProjectUseCaseTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPMate\Tests\Unit\UseCase;
+
+use PHPMate\Domain\Tools\Git\GitRepositoryAuthentication;
+use PHPMate\Domain\Tools\Git\RemoteGitRepository;
+use PHPMate\Infrastructure\Persistence\InMemory\InMemoryProjectsCollection;
+use PHPMate\UseCase\CreateProject;
+use PHPMate\UseCase\CreateProjectUseCase;
+use PHPUnit\Framework\TestCase;
+
+final class CreateProjectUseCaseTest extends TestCase
+{
+    public function testProjectCanBeCreated(): void
+    {
+        $projectsCollection = new InMemoryProjectsCollection();
+
+        self::assertCount(0, $projectsCollection->getAll());
+
+        $handler = new CreateProjectUseCase($projectsCollection);
+        $handler->__invoke(
+            new CreateProject(
+                new RemoteGitRepository(
+                    'https://gitlab.com/phpmate-dogfood/rector.git',
+                    GitRepositoryAuthentication::fromPersonalAccessToken('PAT')
+                )
+            )
+        );
+
+        self::assertCount(1, $projectsCollection->getAll());
+    }
+}
