04f799bd8a5ac4bed6567a31555a2a9d25bb89b8
Jan Mikeš <j.mikes@me.com>
2021-09-03 01:05:36 +0200

    Doctrine mapping for Task

---
+++ src/Infrastructure/Persistence/Doctrine/Type/DoctrineTaskIdType.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMate\Infrastructure\Persistence\Doctrine\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use PHPMate\Domain\Task\TaskId;
+
+final class DoctrineTaskIdType extends Type
+{
+    public const NAME = 'task_id';
+
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getGuidTypeDeclarationSQL($column);
+    }
+
+
+    /**
+     * @param TaskId $value
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
+    {
+        return $value->id;
+    }
+
+
+    /**
+     * @param string $value
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): TaskId
+    {
+        return new TaskId($value);
+    }
+
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+}
