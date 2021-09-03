82532c22fa3196d6e573826ca1bdd5abee5d5e69
Jan Mikeš <j.mikes@me.com>
2021-09-03 17:45:28 +0200

    Fix migration to use json instead

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20210902230353.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20210902230353.php
@@ -20,7 +20,7 @@ final class Version20210902230353 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE task (task_id CHAR(36) NOT NULL COMMENT \'(DC2Type:task_id)\', project_id CHAR(36) NOT NULL COMMENT \'(DC2Type:project_id)\', name VARCHAR(255) NOT NULL, commands LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(task_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE task (task_id CHAR(36) NOT NULL COMMENT \'(DC2Type:task_id)\', project_id CHAR(36) NOT NULL COMMENT \'(DC2Type:project_id)\', name VARCHAR(255) NOT NULL, commands LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', PRIMARY KEY(task_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
