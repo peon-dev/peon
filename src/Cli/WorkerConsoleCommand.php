65c5f8e12cdede823d5d141f6b8e1edf1632089d
Jan Mikeš <j.mikes@me.com>
2021-09-06 22:26:59 +0200

    Example of worker command logic

--- src/Cli/WorkerConsoleCommand.php
+++ src/Cli/WorkerConsoleCommand.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace PHPMate\Cli;
 
+use PHPMate\Domain\Job\JobId;
+use PHPMate\UseCase\ExecuteJob;
+use PHPMate\UseCase\ExecuteJobCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class WorkerConsoleCommand extends Command
 {
-    public function __construct()
-    {
+    public function __construct(
+        private ExecuteJob $executeJobUseCase
+    ) {
         parent::__construct();
     }
 
@@ -26,6 +30,13 @@ final class WorkerConsoleCommand extends Command
     {
         while(true) {
             $output->writeln('Next iteration');
+
+            // Find job that should run
+            $this->executeJobUseCase->handle(
+                new ExecuteJobCommand(
+                    new JobId('')
+                )
+            );
 
             sleep(2);
         }
