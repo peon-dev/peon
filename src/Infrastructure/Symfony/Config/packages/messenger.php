4d19c9e0b787e52fad5a59eef4ce7b20ef44fc0c
Jan Mikeš <j.mikes@me.com>
2021-09-09 23:16:39 +0200

    Use PHP config instead of yaml

---
+++ src/Infrastructure/Symfony/Config/packages/messenger.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+use Symfony\Config\FrameworkConfig;
+
+return static function (FrameworkConfig $framework) {
+    $framework->messenger()->bus('command.bus');
+
+    $framework->messenger()->bus('event.bus')
+        ->defaultMiddleware('allow_no_handlers');
+};
