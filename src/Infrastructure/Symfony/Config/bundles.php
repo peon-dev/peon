709771aa0abd8802c51a4a943a5a20061e1fae94
Jan Mikeš <j.mikes@me.com>
2021-09-15 14:21:00 +0200

    Installed symplify/easy-hydrator

--- src/Infrastructure/Symfony/Config/bundles.php
+++ src/Infrastructure/Symfony/Config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symplify\SimplePhpDocParser\Bundle\SimplePhpDocParserBundle::class => ['all' => true],
+    Symplify\EasyHydrator\EasyHydratorBundle::class => ['all' => true],
 ];
