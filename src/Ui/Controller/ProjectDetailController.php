f23ca4b12002a269d6bc7b83b1c5747f48057a05
Jan Mikeš <j.mikes@me.com>
2021-09-16 01:01:00 +0200

    Styling project detail - guidelines

--- src/Ui/Controller/ProjectDetailController.php
+++ src/Ui/Controller/ProjectDetailController.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 
 namespace PHPMate\Ui\Controller;
 
+use PHPMate\Domain\Project\ProjectId;
+use PHPMate\Domain\Project\ProjectNotFound;
+use PHPMate\Domain\Project\ProjectsCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class ProjectDetailController extends AbstractController
 {
+    public function __construct(
+        private ProjectsCollection $projectsCollection
+    ) {}
+
+
     #[Route(path: '/project/{projectId}', name: 'project_detail')]
     public function __invoke(string $projectId): Response
     {
-        return new Response();
+        try {
+            return $this->render('project_detail.html.twig', [
+                'activeProject' => $this->projectsCollection->get(new ProjectId($projectId)),
+                'tasks' => [],
+            ]);
+        } catch (ProjectNotFound) {
+            throw $this->createNotFoundException();
+        }
     }
 }
