f27d239404a2e37cf2e366e3a38f3a2a34f16faa
Jan Mikeš <j.mikes@me.com>
2021-09-16 00:15:22 +0200

    Added guidelines - create project on dashboard

--- src/Ui/Controller/DashboardController.php
+++ src/Ui/Controller/DashboardController.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace PHPMate\Ui\Controller;
 
-use PHPMate\Domain\Job\JobsCollection;
-use PHPMate\Domain\Project\ProjectsCollection;
-use PHPMate\Domain\Task\TasksCollection;
 use PHPMate\Ui\ReadModel\Dashboard\ProvideReadJobs;
+use PHPMate\Ui\ReadModel\Dashboard\ProvideReadProjects;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +13,8 @@ use Symfony\Component\Routing\Annotation\Route;
 final class DashboardController extends AbstractController
 {
     public function __construct(
-        private JobsCollection $jobsCollection,
-        private ProjectsCollection $projectsCollection,
-        private TasksCollection $tasksCollection,
         private ProvideReadJobs $provideReadJobs,
+        private ProvideReadProjects $provideReadProjects,
     ) {}
 
 
@@ -27,8 +23,7 @@ final class DashboardController extends AbstractController
     {
         return $this->render('dashboard.html.twig', [
             'jobs' => $this->provideReadJobs->provide(10),
-            'projects' => $this->projectsCollection->all(),
-            'tasks' => $this->tasksCollection->all(),
+            'projects' => $this->provideReadProjects->provide(),
         ]);
     }
 }
