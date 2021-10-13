b1642e75c99cc6c6eb871a7abdb9d7a30dd573e1
Jan Mikeš <j.mikes@me.com>
2021-10-14 00:50:38 +0200

    Use prism to format code diffs

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,10 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
+require('prismjs');
+require('prismjs/components/prism-diff');
+require('prismjs/themes/prism.css');
+
 require('bootstrap');
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
