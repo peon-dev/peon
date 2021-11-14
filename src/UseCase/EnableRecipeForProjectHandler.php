eb82b1b35ffce255c8f1e2efd3a65ac22b937729
Jan Mikeš <j.mikes@me.com>
2021-11-14 23:24:39 +0100

    UseCase: Enable Recipe For Project - Check that recipe exists

--- src/UseCase/EnableRecipeForProjectHandler.php
+++ src/UseCase/EnableRecipeForProjectHandler.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PHPMate\UseCase;
 
+use PHPMate\Domain\Cookbook\RecipeNotFound;
+use PHPMate\Domain\Cookbook\RecipesCollection;
 use PHPMate\Domain\Project\ProjectNotFound;
 use PHPMate\Domain\Project\ProjectsCollection;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
@@ -11,15 +13,21 @@ use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 final class EnableRecipeForProjectHandler  implements MessageHandlerInterface
 {
     public function __construct(
-        private ProjectsCollection $projectsCollection
+        private ProjectsCollection $projectsCollection,
+        private RecipesCollection $recipesCollection
     ) {}
 
 
     /**
      * @throws ProjectNotFound
+     * @throws RecipeNotFound
      */
     public function __invoke(EnableRecipeForProject $command): void
     {
+        if ($this->recipesCollection->hasRecipeWithName($command->recipeName) === false) {
+            throw new RecipeNotFound();
+        }
+
         $project = $this->projectsCollection->get($command->projectId);
 
         $project->enableRecipe($command->recipeName);
