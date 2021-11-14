d7704be965050b07dd291578eea7466b758f072d
Jan Mikeš <j.mikes@me.com>
2021-11-14 22:24:50 +0100

    Added empty EnableRecipeForProject use case

---
+++ tests/Unit/UseCase/EnableRecipeForProjectHandlerTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPMate\Tests\Unit\UseCase;
+
+use PHPUnit\Framework\TestCase;
+
+class EnableRecipeForProjectHandlerTest extends TestCase
+{
+    public function testRecipeCanBeEnabled(): void
+    {
+    }
+
+    public function testProjectNotFound(): void
+    {
+    }
+
+    public function testRecipeNotFound(): void
+    {
+    }
+
+    public function testRecipeAlreadyEnabled(): void
+    {
+    }
+}
