a7c4bedbe77c1b045396cd360bfc4cdc92e38621
Jan Mikeš <j.mikes@me.com>
2021-11-18 13:58:28 +0100

    Added test for DoctrineJobsCollection

--- tests/Integration/Infrastructure/Persistence/Doctrine/DoctrineJobsCollectionTest.php
+++ tests/Integration/Infrastructure/Persistence/Doctrine/DoctrineJobsCollectionTest.php
@@ -3,13 +3,19 @@ declare(strict_types=1);
 
 namespace PHPMate\Tests\Integration\Infrastructure\Persistence\Doctrine;
 
+use Lcobucci\Clock\Clock;
+use PHPMate\Domain\Job\Job;
+use PHPMate\Domain\Project\ProjectId;
+use PHPMate\Domain\Task\TaskId;
 use PHPMate\Infrastructure\Persistence\Doctrine\DoctrineJobsCollection;
-use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 final class DoctrineJobsCollectionTest extends KernelTestCase
 {
     private DoctrineJobsCollection $doctrineJobsCollection;
+
+    private Clock $clock;
 
 
     protected function setUp(): void
@@ -17,11 +23,30 @@ final class DoctrineJobsCollectionTest extends KernelTestCase
         $container = self::getContainer();
 
         $this->doctrineJobsCollection = $container->get(DoctrineJobsCollection::class);
+        $this->clock = $container->get(Clock::class);
     }
 
 
-    public function testPersistence(): void
+    public function testPersistenceWorks(): void
     {
-        $this->doctrineJobsCollection->all();
+        self::assertCount(0, $this->doctrineJobsCollection->all());
+
+        $jobId = $this->doctrineJobsCollection->nextIdentity();
+
+        // TODO: consider using some kind of factory
+        $job = new Job(
+            $jobId,
+            new ProjectId(Uuid::uuid4()->toString()),
+            new TaskId(Uuid::uuid4()->toString()),
+            'Task name',
+            $this->clock,
+            ['command']
+        );
+
+        $this->doctrineJobsCollection->save($job);
+
+        self::assertCount(1, $this->doctrineJobsCollection->all());
+
+        $this->doctrineJobsCollection->get($jobId);
     }
 }
