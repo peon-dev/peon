8be73c7813bd19cc149685ab9ba47092dff4aebc
Jan Mikeš <j.mikes@me.com>
2021-11-20 14:34:06 +0100

    Rename database columns to better reflect reality

--- src/Ui/ReadModel/Dashboard/ProvideReadJobs.php
+++ src/Ui/ReadModel/Dashboard/ProvideReadJobs.php
@@ -24,12 +24,12 @@ final class ProvideReadJobs
 SELECT 
     job.job_id, job.project_id, job.task_id, job.task_name, job.started_at, job.succeeded_at, job.failed_at, 
     project.name as project_name,
-    SUM(job_process.result_execution_time) as execution_time
+    SUM(job_process_result.execution_time) as execution_time
 FROM job
 JOIN project ON project.project_id = job.project_id
 JOIN task ON task.task_id = job.task_id
-LEFT JOIN job_process ON job.job_id = job_process.job_id
-GROUP BY job.job_id, job_process.job_id, project.name, job.scheduled_at
+LEFT JOIN job_process_result ON job.job_id = job_process_result.job_id
+GROUP BY job.job_id, job_process_result.job_id, project.name, job.scheduled_at
 ORDER BY job.scheduled_at DESC
 LIMIT ?
 SQL;
