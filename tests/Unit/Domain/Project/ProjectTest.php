6ce13ce8a8a6ecef7eff9634fcfc7a26d113a882
Jan Mikeš <j.mikes@me.com>
2021-11-20 13:04:01 +0100

    Improving Project unit tests

--- tests/Unit/Domain/Project/ProjectTest.php
+++ tests/Unit/Domain/Project/ProjectTest.php
@@ -14,44 +14,58 @@ use PHPUnit\Framework\TestCase;
 
 class ProjectTest extends TestCase
 {
+    public function testProjectNameWillBeTakenFromRepository(): void
+    {
+        $project = $this->createProject();
+
+        self::assertSame('phpmate-dogfood/rector', $project->name);
+    }
+
+
     public function testEnableRecipe(): void
     {
-        $project = new Project(
-            new ProjectId(''),
-            new RemoteGitRepository(
-                'https://gitlab.com/phpmate-dogfood/rector.git',
-                GitRepositoryAuthentication::fromPersonalAccessToken('PAT')
-            )
-        );
+        $project = $this->createProject();
 
         self::assertCount(0, $project->enabledRecipes);
-        $project->enableRecipe(new RecipeName('test'));
+
+        $recipeName = new RecipeName('test');
+        $project->enableRecipe($recipeName);
+
         self::assertCount(1, $project->enabledRecipes);
+        self::assertTrue($recipeName->isEqual($project->enabledRecipes[0]));
 
         $this->expectException(RecipeAlreadyEnabledForProject::class);
-
-        $project->enableRecipe(new RecipeName('test'));
+        $project->enableRecipe($recipeName);
     }
 
 
     public function testDisableRecipe(): void
     {
-        $project = new Project(
+        $project = $this->createProject();
+        $toBeDisabledRecipeName = new RecipeName('to-be-removed');
+
+        $project->enableRecipe($toBeDisabledRecipeName);
+        $project->enableRecipe(new RecipeName('test'));
+        self::assertCount(2, $project->enabledRecipes);
+        self::assertTrue($toBeDisabledRecipeName->isEqual($project->enabledRecipes[array_key_first($project->enabledRecipes)]));
+
+        $project->disableRecipe($toBeDisabledRecipeName);
+        self::assertCount(1, $project->enabledRecipes);
+        self::assertFalse($toBeDisabledRecipeName->isEqual($project->enabledRecipes[array_key_first($project->enabledRecipes)]));
+
+        $this->expectException(RecipeNotEnabledForProject::class);
+        $project->disableRecipe($toBeDisabledRecipeName);
+    }
+
+
+    private function createProject(): Project
+    {
+        return new Project(
             new ProjectId(''),
             new RemoteGitRepository(
                 'https://gitlab.com/phpmate-dogfood/rector.git',
                 GitRepositoryAuthentication::fromPersonalAccessToken('PAT')
             )
         );
-
-        $project->enableRecipe(new RecipeName('test'));
-
-        self::assertCount(1, $project->enabledRecipes);
-        $project->disableRecipe(new RecipeName('test'));
-        self::assertCount(0, $project->enabledRecipes);
-
-        $this->expectException(RecipeNotEnabledForProject::class);
-
-        $project->disableRecipe(new RecipeName('test'));
     }
 }
