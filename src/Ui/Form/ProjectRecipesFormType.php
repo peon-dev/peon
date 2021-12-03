238fa19dab81b3f4acf90cbfb9e18638fbaae594
Jan Mikeš <j.mikes@me.com>
2021-12-03 14:13:40 +0100

    Project recipes form: recipes source is now dynamic

--- src/Ui/Form/ProjectRecipesFormType.php
+++ src/Ui/Form/ProjectRecipesFormType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PHPMate\Ui\Form;
 
+use PHPMate\Infrastructure\Cookbook\StaticRecipesCollection;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,12 +14,16 @@ final class ProjectRecipesFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // TODO: get rid of this ugly static, temporary hack
+        $recipesCollection = new StaticRecipesCollection();
+
+        $choices = [];
+        foreach ($recipesCollection->all() as $recipe) {
+            $choices[$recipe->title] = $recipe->name->toString();
+        }
+
         $builder->add('recipes', ChoiceType::class, [
-           'choices' => [
-               // TODO: we need dynamic choices here :-)
-               'Unused private methods' => 'unused-private-methods',
-               'Typed properties' => 'typed-properties',
-           ],
+           'choices' => $choices,
             'expanded' => true,
             'multiple' => true,
         ]);
