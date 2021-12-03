1c7ecbac72272d5d8a8994077d34949b50025317
Jan Mikeš <j.mikes@me.com>
2021-12-03 14:23:46 +0100

    Recipes in cookbook are now dynamic

--- src/Ui/Controller/CookbookController.php
+++ src/Ui/Controller/CookbookController.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace PHPMate\Ui\Controller;
 
+use PHPMate\Domain\Cookbook\RecipesCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class CookbookController extends AbstractController
 {
+    public function __construct(
+        private RecipesCollection $recipesCollection
+    ) {}
+
+
     #[Route(path: '/cookbook', name: 'cookbook')]
     public function __invoke(): Response
     {
-        return $this->render('cookbook.html.twig');
+        return $this->render('cookbook.html.twig', [
+            'recipes' => $this->recipesCollection->all(),
+        ]);
     }
 }
