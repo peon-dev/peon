4ca8c4a45ed94a01fe39621567f13568aa7af10b
Jan Mikeš <j.mikes@me.com>
2021-12-03 14:47:00 +0100

    UI: Enabled recipes in project detail are now in table

--- src/Ui/Controller/ProjectDetailController.php
+++ src/Ui/Controller/ProjectDetailController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PHPMate\Ui\Controller;
 
+use PHPMate\Domain\Cookbook\RecipesCollection;
 use PHPMate\Domain\Project\Value\ProjectId;
 use PHPMate\Domain\Project\Exception\ProjectNotFound;
 use PHPMate\Domain\Project\ProjectsCollection;
@@ -16,7 +17,8 @@ final class ProjectDetailController extends AbstractController
 {
     public function __construct(
         private ProjectsCollection $projectsCollection,
-        private ProvideReadTasks $provideReadTasks
+        private ProvideReadTasks $provideReadTasks,
+        private RecipesCollection $recipesCollection,
     ) {}
 
 
@@ -27,6 +29,7 @@ final class ProjectDetailController extends AbstractController
             return $this->render('project_detail.html.twig', [
                 'activeProject' => $this->projectsCollection->get(new ProjectId($projectId)),
                 'tasks' => $this->provideReadTasks->provide($projectId),
+                'recipesCollection' => $this->recipesCollection, // TODO: this should not be in template, remove later, ugly pattern
             ]);
         } catch (ProjectNotFound) {
             throw $this->createNotFoundException();
