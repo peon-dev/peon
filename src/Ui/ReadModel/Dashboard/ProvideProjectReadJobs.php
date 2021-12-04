dc76cf1083d53bc6db423e62c9ad573b988b26e5
Jan Mikeš <j.mikes@me.com>
2021-12-04 20:18:28 +0100

    Show recent jobs on project page

---
+++ src/Ui/ReadModel/Dashboard/ProvideProjectReadJobs.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMate\Ui\ReadModel\Dashboard;
+
+use Doctrine\DBAL\Connection;
+use PHPMate\Domain\Project\Value\ProjectId;
+use Symplify\EasyHydrator\ArrayToValueObjectHydrator;
+
+final class ProvideProjectReadJobs
+{
+    public function __construct(
+        private Connection $connection,
+        private ArrayToValueObjectHydrator $hydrator,
+    ) {}
+
+
+    /**
+     * @return array<ReadJob>
+     */
+    public function provide(ProjectId $projectId, int $maxJobsLimit): array
+    {
+        $sql = <<<SQL
+SELECT 
+    job.job_id, job.project_id, job.task_id, job.title, job.started_at, job.succeeded_at, job.failed_at,
+    project.name as project_name,
+    SUM(job_process_result.execution_time) as execution_time
+FROM job
+JOIN project ON project.project_id = job.project_id
+LEFT JOIN task ON task.task_id = job.task_id
+LEFT JOIN job_process_result ON job.job_id = job_process_result.job_id
+WHERE job.project_id = ?
+GROUP BY job.job_id, job_process_result.job_id, project.name, job.scheduled_at
+ORDER BY job.scheduled_at DESC
+LIMIT ?
+SQL;
+
+        $resultSet = $this->connection->executeQuery($sql, [$projectId->id, $maxJobsLimit], ['string', 'integer']);
+
+        return $this->hydrator->hydrateArrays($resultSet->fetchAllAssociative(), ReadJob::class);
+    }
+}
