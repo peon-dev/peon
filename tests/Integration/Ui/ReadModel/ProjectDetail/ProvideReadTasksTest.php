8701a37cdf977ca6d089640498499aec2bee074a
Jan Mikeš <j.mikes@me.com>
2021-12-06 00:28:50 +0100

    Added test for provide read tasks

--- tests/Integration/Ui/ReadModel/ProjectDetail/ProvideReadTasksTest.php
+++ tests/Integration/Ui/ReadModel/ProjectDetail/ProvideReadTasksTest.php
@@ -3,13 +3,37 @@ declare(strict_types=1);
 
 namespace PHPMate\Tests\Integration\Ui\ReadModel\ProjectDetail;
 
+use PHPMate\Domain\Project\Value\ProjectId;
+use PHPMate\Tests\DataFixtures\DataFixtures;
 use PHPMate\Ui\ReadModel\ProjectDetail\ProvideReadTasks;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-final class ProvideReadTasksTest extends TestCase
+final class ProvideReadTasksTest extends KernelTestCase
 {
+    private ProvideReadTasks $provideReadTasksRecipes;
+
+
+    protected function setUp(): void
+    {
+        $container = self::getContainer();
+
+        $this->provideReadTasksRecipes = $container->get(ProvideReadTasks::class);
+    }
+
+
     public function testItWorks(): void
     {
-        
+        /**
+         * Data are populated from data fixtures
+         * @see \PHPMate\Tests\DataFixtures\DataFixtures
+         */
+        $projectId = DataFixtures::PROJECT_ID;
+
+        $readTasks = $this->provideReadTasksRecipes->provide(new ProjectId($projectId));
+
+        self::assertCount(1, $readTasks);
+
+        $task = $readTasks[0];
+        self::assertSame(DataFixtures::TASK_ID, $task->taskId);
     }
 }
