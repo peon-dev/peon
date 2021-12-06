aca8757f67db88bdfac72cee29c6e04269880826
Jan Mikeš <j.mikes@me.com>
2021-12-06 12:09:51 +0100

    Added failing tests for enable+disable project recipes

---
+++ tests/Application/Ui/Controller/EnableRecipeForProjectControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPMate\Tests\Application\Ui\Controller;
+
+use PHPMate\Domain\Cookbook\Value\RecipeName;
+use PHPMate\Domain\Project\ProjectsCollection;
+use PHPMate\Domain\Project\Value\ProjectId;
+use PHPMate\Tests\DataFixtures\DataFixtures;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class EnableRecipeForProjectControllerTest extends WebTestCase
+{
+    public function testNonExistingProjectWillShow404(): void
+    {
+        $client = self::createClient();
+        $projectId = '00000000-0000-0000-0000-000000000000';
+        $recipeName = RecipeName::SWITCH_TO_MATCH;
+
+        $client->request('GET', "/projects/$projectId/recipe/$recipeName/enable");
+
+        self::assertResponseStatusCodeSame(404);
+    }
+
+
+    public function testNonExistingRecipeWillShow404(): void
+    {
+        $client = self::createClient();
+        $projectId = DataFixtures::PROJECT_ID;
+        $recipeName = 'something-not-existing';
+
+        $client->request('GET', "/projects/$projectId/recipe/$recipeName/enable");
+
+        self::assertResponseStatusCodeSame(404);
+    }
+
+
+    public function testAlreadyEnabledRecipeWillChangeNothing(): void
+    {
+        $client = self::createClient();
+        $container = self::getContainer();
+        $projectsCollection = $container->get(ProjectsCollection::class);
+        $projectId = DataFixtures::PROJECT_ID;
+        $recipeName = RecipeName::TYPED_PROPERTIES;
+
+        $project = $projectsCollection->get(new ProjectId($projectId));
+        $enabledRecipesCountBeforeScenario = $project->enabledRecipes;
+
+        $client->request('GET', "/projects/$projectId/recipe/$recipeName/enable");
+
+        self::assertResponseRedirects("/projects/$projectId/cookbook");
+
+        $project = $projectsCollection->get(new ProjectId($projectId));
+        self::assertCount(count($enabledRecipesCountBeforeScenario), $project->enabledRecipes);
+    }
+
+
+    public function testRecipeWillBeEnabled(): void
+    {
+        $client = self::createClient();
+        $container = self::getContainer();
+        $projectsCollection = $container->get(ProjectsCollection::class);
+        $projectId = DataFixtures::PROJECT_ID;
+        $recipeName = RecipeName::SWITCH_TO_MATCH;
+
+        $project = $projectsCollection->get(new ProjectId($projectId));
+        $enabledRecipesCountBeforeScenario = $project->enabledRecipes;
+
+        $client->request('GET', "/projects/$projectId/recipe/$recipeName/enable");
+
+        self::assertResponseRedirects("/projects/$projectId/cookbook");
+
+        $project = $projectsCollection->get(new ProjectId($projectId));
+        self::assertCount(1 + count($enabledRecipesCountBeforeScenario), $project->enabledRecipes);
+    }
+}
