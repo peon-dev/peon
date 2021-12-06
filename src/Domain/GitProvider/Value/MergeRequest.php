237d5e495542141a8a1182f9bbcd3385b1eafa38
Jan Mikeš <j.mikes@me.com>
2021-12-06 23:33:35 +0100

    Remove final from merge request to prevent doctrine issues

--- src/Domain/GitProvider/Value/MergeRequest.php
+++ src/Domain/GitProvider/Value/MergeRequest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PHPMate\Domain\GitProvider\Value;
 
-final class MergeRequest
+class MergeRequest
 {
     public function __construct(
         public readonly string|null $url,
