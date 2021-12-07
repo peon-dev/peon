ae3f8e59ee614400a9541f04b866bca951fe8daa
Jan Mikeš <j.mikes@me.com>
2021-12-07 01:26:10 +0100

    Fix broken tooltips

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 import '@popperjs/core'
 import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
@@ -8,4 +9,8 @@ import './styles/app.scss';
 import './bootstrap';
 import './highlight'
 
-const $ = require('jquery');
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
