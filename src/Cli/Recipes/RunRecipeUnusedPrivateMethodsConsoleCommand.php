03b3f3af1944ae66185c1d8e5c0cb7ef81222a8c
Jan Mikeš <j.mikes@me.com>
2021-12-08 00:29:28 +0100

    Fix phpstan warnings

--- src/Cli/Recipes/RunRecipeUnusedPrivateMethodsConsoleCommand.php
+++ src/Cli/Recipes/RunRecipeUnusedPrivateMethodsConsoleCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PHPMate\Cli\Recipes;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,7 +12,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class RunRecipeUnusedPrivateMethodsConsoleCommand extends Command
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
     ) {
         parent::__construct('phpmate:run-recipe:unused-private-methods');
     }
@@ -27,9 +25,10 @@ final class RunRecipeUnusedPrivateMethodsConsoleCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln(
-            $input->getArgument('application_path')
-        );
+        $applicationPath = $input->getArgument('application_path');
+        assert(is_string($applicationPath));
+
+        $output->writeln($applicationPath);
 
         return self::SUCCESS;
     }
