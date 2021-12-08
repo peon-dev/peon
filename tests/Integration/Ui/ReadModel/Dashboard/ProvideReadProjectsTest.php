f94639ed62b003b42e0d96d8da4265a02a9fdc09
Jan Mikeš <j.mikes@me.com>
2021-12-08 23:28:24 +0100

    Fix provide read projects test

--- tests/Integration/Ui/ReadModel/Dashboard/ProvideReadProjectsTest.php
+++ tests/Integration/Ui/ReadModel/Dashboard/ProvideReadProjectsTest.php
@@ -31,7 +31,12 @@ final class ProvideReadProjectsTest extends KernelTestCase
         self::assertCount(2, $readProjects);
 
         $readProject = $readProjects[0];
+        self::assertSame(DataFixtures::PROJECT_2_ID, $readProject->projectId);
+        self::assertSame(0, $readProject->tasksCount);
+        self::assertSame(0, $readProject->jobsCount);
+        self::assertSame(0, $readProject->recipesCount);
 
+        $readProject = $readProjects[1];
         self::assertSame(DataFixtures::PROJECT_1_ID, $readProject->projectId);
         self::assertSame(1, $readProject->tasksCount);
         self::assertSame(3, $readProject->jobsCount);
