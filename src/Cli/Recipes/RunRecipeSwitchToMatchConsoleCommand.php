8c0ddc1d0cb7fff317aebea93109366244b13794
Jan Mikeš <j.mikes@me.com>
2021-12-08 21:22:38 +0100

    New recipes: SwitchToMatch, UnusedPrivateMehods

--- src/Cli/Recipes/RunRecipeSwitchToMatchConsoleCommand.php
+++ src/Cli/Recipes/RunRecipeSwitchToMatchConsoleCommand.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace PHPMate\Cli\Recipes;
 
+use PHPMate\Domain\Process\ProcessLogger;
+use PHPMate\Domain\Tools\Rector\Rector;
+use PHPMate\Domain\Tools\Rector\Value\RectorProcessCommandConfiguration;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,6 +15,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class RunRecipeSwitchToMatchConsoleCommand extends Command
 {
     public function __construct(
+        private Rector $rector,
+        private ProcessLogger $processLogger,
     ) {
         parent::__construct('phpmate:run-recipe:switch-to-match');
     }
@@ -29,6 +34,22 @@ final class RunRecipeSwitchToMatchConsoleCommand extends Command
         assert(is_string($applicationPath));
 
         $output->writeln($applicationPath);
+
+        // TODO: detect PSR-4 roots
+        $paths = ['src'];
+
+        $configuration = new RectorProcessCommandConfiguration(
+            autoloadFile: $applicationPath . '/vendor/autoload.php',
+            config: __DIR__ . '/../../../vendor-bin/rector/config/switch-to-match.php',
+            paths: $paths,
+        );
+
+        $this->rector->process($applicationPath, $configuration);
+
+        // TODO: this could be improved, maybe?
+        foreach ($this->processLogger->popLogs() as $log) {
+            $output->writeln($log->output);
+        }
 
         return self::SUCCESS;
     }
--- src/Cli/Recipes/RunRecipeTypedPropertiesConsoleCommand.php
+++ src/Cli/Recipes/RunRecipeTypedPropertiesConsoleCommand.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace PHPMate\Cli\Recipes;
 
+use PHPMate\Domain\Process\ProcessLogger;
+use PHPMate\Domain\Tools\Rector\Rector;
+use PHPMate\Domain\Tools\Rector\Value\RectorProcessCommandConfiguration;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,6 +15,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class RunRecipeTypedPropertiesConsoleCommand extends Command
 {
     public function __construct(
+        private Rector $rector,
+        private ProcessLogger $processLogger,
     ) {
         parent::__construct('phpmate:run-recipe:typed-properties');
     }
@@ -29,6 +34,22 @@ final class RunRecipeTypedPropertiesConsoleCommand extends Command
         assert(is_string($applicationPath));
 
         $output->writeln($applicationPath);
+
+        // TODO: detect PSR-4 roots
+        $paths = ['src'];
+
+        $configuration = new RectorProcessCommandConfiguration(
+            autoloadFile: $applicationPath . '/vendor/autoload.php',
+            config: __DIR__ . '/../../../vendor-bin/rector/config/typed-properties.php',
+            paths: $paths,
+        );
+
+        $this->rector->process($applicationPath, $configuration);
+
+        // TODO: this could be improved, maybe?
+        foreach ($this->processLogger->popLogs() as $log) {
+            $output->writeln($log->output);
+        }
 
         return self::SUCCESS;
     }
