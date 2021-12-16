4db2011cf510055b73041708e682da7050e0cdb9
Jan Mikeš <j.mikes@me.com>
2021-12-16 01:16:33 +0100

    Fix migration

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20211216001614.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20211216001614.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211215233410 extends AbstractMigration
+final class Version20211216001614 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,6 +21,7 @@ final class Version20211215233410 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE project ADD baselines JSON DEFAULT \'[]\' NOT NULL');
+        $this->addSql('COMMENT ON COLUMN project.baselines IS \'(DC2Type:recipe_baselines_array)\'');
     }
 
     public function down(Schema $schema): void
