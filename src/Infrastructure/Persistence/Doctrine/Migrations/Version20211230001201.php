03f9a9936b8c372163bfad8512643fd5a98064e2
Jan Mikeš <j.mikes@me.com>
2021-12-30 01:14:04 +0100

    Change db type for job command

---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20211230001201.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMate\DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211230001201 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE job_process_result ALTER command TYPE TEXT');
+        $this->addSql('ALTER TABLE job_process_result ALTER command DROP DEFAULT');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE job_process_result ALTER command TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE job_process_result ALTER command DROP DEFAULT');
+    }
+}
