bc49bf466afb02504aee3942539a37815439b8a5
Jan Mikeš <j.mikes@me.com>
2021-12-30 01:09:14 +0100

    Tracy - for local development

---
+++ src/Infrastructure/Symfony/Config/packages/dev/tracy.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Mangoweb\MonologTracyHandler\TracyHandler;
+use Mangoweb\MonologTracyHandler\TracyProcessor;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->autoconfigure()
+        ->autowire();
+
+    $services->set(TracyProcessor::class);
+
+    $services->set(TracyHandler::class)
+        ->args(['$localBlueScreenDirectory' => '%kernel.logs_dir%']);
+};
+
+
