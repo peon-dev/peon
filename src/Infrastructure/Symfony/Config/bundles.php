e3e10a9dbec72ca4fc8b1ba50061ae47530fc5fb
Jan Mikeš <j.mikes@me.com>
2021-12-30 20:55:18 +0100

    Install turbo!

--- src/Infrastructure/Symfony/Config/bundles.php
+++ src/Infrastructure/Symfony/Config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
