dd20b1c78ee4c1e3d0956f3c9c74d8a9da5393ad
Jan Mikeš <j.mikes@me.com>
2022-01-09 17:32:18 +0100

    Publish changes to cookbook when recipe enabled/disabled

--- src/Subscribers/PublishMercureUpdateWhenRecipeEnabled.php
+++ src/Subscribers/PublishMercureUpdateWhenRecipeEnabled.php
@@ -6,6 +6,8 @@ namespace PHPMate\Subscribers;
 
 use PHPMate\Domain\Cookbook\Event\RecipeEnabled;
 use PHPMate\Packages\MessageBus\Event\EventHandlerInterface;
+use PHPMate\Ui\ReadModel\ProjectDetail\ProvideProjectReadRecipes;
+use PHPMate\Ui\ReadModel\ProjectDetail\ProvideReadProjectDetail;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Twig\Environment;
@@ -13,18 +15,37 @@ use Twig\Environment;
 final class PublishMercureUpdateWhenRecipeEnabled implements EventHandlerInterface
 {
     public function __construct(
-        private HubInterface $hub,
-        private Environment $twig,
+        private HubInterface              $hub,
+        private Environment               $twig,
+        private ProvideProjectReadRecipes $provideProjectReadRecipes,
+        private ProvideReadProjectDetail  $provideReadProjectDetail,
     ) {}
 
 
     public function __invoke(RecipeEnabled $event): void
     {
+        $project = $this->provideReadProjectDetail->provide($event->projectId);
+
         // Dashboard - project stats
-        // Project overview - recipes table
 
-        // $update = new Update();
+        $this->hub->publish(
+            new Update(
+                'project-' . $event->projectId->id . '-cookbook',
+                $this->twig->render('cookbook.stream.html.twig', [
+                    'project' => $project,
+                    'recipeName' => $event->recipeName
+                ])
+            )
+        );
 
-        // $this->hub->publish($update);
+        $this->hub->publish(
+            new Update(
+                'project-' . $event->projectId->id . '-overview',
+                $this->twig->render('recipes.stream.html.twig', [
+                    'projectId' => $event->projectId,
+                    'recipes' => $this->provideProjectReadRecipes->provide($event->projectId),
+                ])
+            )
+        );
     }
 }
