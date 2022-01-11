fb8ae4287308184787ad35f71474ba5dc2330a3b
Jan Mikeš <j.mikes@me.com>
2022-01-11 10:42:33 +0100

    Use dummy hub for tests

---
+++ src/Infrastructure/Mercure/DummyHub.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMate\Infrastructure\Mercure;
+
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Jwt\TokenFactoryInterface;
+use Symfony\Component\Mercure\Jwt\TokenProviderInterface;
+use Symfony\Component\Mercure\Update;
+
+final class DummyHub implements HubInterface
+{
+    public function getUrl(): string
+    {
+        return 'https://internal/.well-known/mercure';
+    }
+
+
+    public function getPublicUrl(): string
+    {
+        return 'https://external/.well-known/mercure';
+    }
+
+
+    public function getProvider(): TokenProviderInterface
+    {
+        return new class implements TokenProviderInterface {
+            public function getJwt(): string
+            {
+                return '';
+            }
+        };
+    }
+
+
+    public function getFactory(): ?TokenFactoryInterface
+    {
+        return null;
+    }
+
+
+    public function publish(Update $update): string
+    {
+        return 'id';
+    }
+}
