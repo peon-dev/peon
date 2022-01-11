e4e60e45fd130bc9af707872018f5a3334925368
Jan Mikeš <j.mikes@me.com>
2022-01-11 09:43:22 +0100

    Dashboard: project stats realtime update for tasks

--- src/Subscribers/PublishMercureUpdateWhenTaskAdded.php
+++ src/Subscribers/PublishMercureUpdateWhenTaskAdded.php
@@ -6,6 +6,7 @@ namespace PHPMate\Subscribers;
 
 use PHPMate\Domain\Task\Event\TaskAdded;
 use PHPMate\Packages\MessageBus\Event\EventHandlerInterface;
+use PHPMate\Ui\ReadModel\Dashboard\ProvideReadProjectById;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Twig\Environment;
@@ -15,13 +16,23 @@ final class PublishMercureUpdateWhenTaskAdded implements EventHandlerInterface
     public function __construct(
         private HubInterface $hub,
         private Environment $twig,
+        private ProvideReadProjectById $provideReadProjectById,
     ) {}
 
 
     public function __invoke(TaskAdded $event): void
     {
-        // Dashboard - project stats
         // Project overview - tasks table append
 
+        $project = $this->provideReadProjectById->provide($event->projectId);
+
+        $this->hub->publish(
+            new Update(
+                'dashboard',
+                $this->twig->render('dashboard.project_stats.stream.html.twig', [
+                    'project' => $project,
+                ])
+            )
+        );
     }
 }
