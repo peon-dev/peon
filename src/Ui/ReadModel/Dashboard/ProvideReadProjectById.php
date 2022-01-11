3be9b056c0d5fabf5872040666cc2ce4397e0c8c
Jan Mikeš <j.mikes@me.com>
2022-01-11 09:37:59 +0100

    Dashboard: project stats realtime update for jobs and recipes

---
+++ src/Ui/ReadModel/Dashboard/ProvideReadProjectById.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPMate\Ui\ReadModel\Dashboard;
+
+use Doctrine\DBAL\Connection;
+use PHPMate\Domain\Project\Value\ProjectId;
+use Symplify\EasyHydrator\ArrayToValueObjectHydrator;
+
+final class ProvideReadProjectById // TODO: test
+{
+    public function __construct(
+        private Connection $connection,
+        private ArrayToValueObjectHydrator $hydrator,
+    ) {}
+
+
+    public function provide(ProjectId $projectId): ReadProject
+    {
+        $sql = <<<SQL
+SELECT
+       project.project_id, project.name,
+       count(DISTINCT task.task_id) as tasks_count,
+       count(DISTINCT job.job_id) as jobs_count,
+       json_array_length(project.enabled_recipes) as recipes_count
+FROM project
+LEFT JOIN job ON job.project_id = project.project_id
+LEFT JOIN task ON task.project_id = project.project_id
+WHERE project.project_id = ?
+GROUP BY project.project_id
+ORDER BY project.name
+SQL;
+
+        $resultSet = $this->connection->executeQuery($sql, [$projectId]);
+
+        return $this->hydrator->hydrateArray($resultSet->fetchAssociative(), ReadProject::class);
+    }
+}
