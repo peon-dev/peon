44bb3a55d06a41be3c3388e5aa450dd36ce6c5e7
Jan Mikeš <j.mikes@me.com>
2022-01-13 17:06:36 +0100

    WIP

--- src/Cli/SchedulerConsoleCommand.php
+++ src/Cli/SchedulerConsoleCommand.php
@@ -96,6 +96,15 @@ SQL;
         // TODO: find better way, how to make it in 1 SQL
 
         $sql = <<<SQL
+SELECT project.project_id, recipe_name, MAX(scheduled_at) AS last_schedule
+FROM project
+CROSS JOIN LATERAL (SELECT json_array_elements(project.enabled_recipes)->>'recipe_name' AS recipe_name) enabled_recipe
+LEFT JOIN job ON job.project_id = project.project_id AND job.enabled_recipe->>'recipe_name' = recipe_name
+GROUP BY project.project_id, recipe_name;
+SQL;
+
+
+        $sql = <<<SQL
 SELECT project.project_id, unnest(project.enabled_recipes) AS enabled_recipe_name
 FROM project
 SQL;
