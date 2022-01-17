e97a17f04ef5aa4e04b9ed8f2bdef376a5202169
Jan Mikeš <j.mikes@me.com>
2022-01-17 21:43:33 +0100

    Prepared tests

---
+++ tests/Unit/UseCase/ScheduleRecipesHandlerTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Peon\Tests\Unit\UseCase;
+
+use Peon\UseCase\ScheduleRecipesHandler;
+use PHPUnit\Framework\TestCase;
+
+final class ScheduleRecipesHandlerTest extends TestCase
+{
+
+}
---
+++ tests/Unit/UseCase/ScheduleTasksHandlerTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Peon\Tests\Unit\UseCase;
+
+use Peon\UseCase\ScheduleTasksHandler;
+use PHPUnit\Framework\TestCase;
+
+final class ScheduleTasksHandlerTest extends TestCase
+{
+
+}
