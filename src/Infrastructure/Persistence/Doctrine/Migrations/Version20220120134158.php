6f4602a9650df9c2be40fe6c54b534c480b55739
Jan Mikeš <j.mikes@me.com>
2022-01-20 14:48:43 +0100

    Doctrine migration for project build configuration

---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20220120134158.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peon\DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220120134158 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE project ADD build_configuration JSON DEFAULT \'{}\' NOT NULL');
+        $this->addSql('COMMENT ON COLUMN project.build_configuration IS \'(DC2Type:build_configuration)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE project DROP build_configuration');
+    }
+}
