0b166ddc04d069b921b3162bab07f0cb1be76eb0
Jan Mikeš <j.mikes@me.com>
2022-01-20 22:46:24 +0100

    Application test for project configuration

--- tests/Application/Ui/Controller/ProjectSettingsControllerTest.php
+++ tests/Application/Ui/Controller/ProjectSettingsControllerTest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Peon\Tests\Application\Ui\Controller;
 
+use Peon\Domain\Project\ProjectsCollection;
+use Peon\Domain\Project\Value\ProjectId;
 use Peon\Tests\DataFixtures\DataFixtures;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -27,5 +29,32 @@ final class ProjectSettingsControllerTest extends WebTestCase
         $client->request('GET', "/projects/$projectId/settings");
 
         self::assertResponseIsSuccessful();
+    }
+
+
+    public function testProjectCanBeConfiguredBySubmittingForm(): void
+    {
+        $client = self::createClient();
+        $container = self::getContainer();
+        $projectsCollection = $container->get(ProjectsCollection::class);
+        $projectId = DataFixtures::PROJECT_1_ID;
+
+        // Check it was false before
+        $project = $projectsCollection->get(new ProjectId($projectId));
+        self::assertFalse($project->buildConfiguration->skipComposerInstall);
+
+        $crawler = $client->request('GET', "/projects/$projectId/settings");
+
+        $form = $crawler->selectButton('submit')->form();
+
+        $client->submit($form, [
+            $form->getName() . '[skipComposerInstall]' => true,
+        ]);
+
+        self::assertResponseRedirects("/projects/$projectId");
+
+        // Check it is true after submitting
+        $project = $projectsCollection->get(new ProjectId($projectId));
+        self::assertTrue($project->buildConfiguration->skipComposerInstall);
     }
 }
