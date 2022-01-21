e6db17a8e0582338066353163db0c0e80a684c4f
Jan Mikeš <j.mikes@me.com>
2022-01-21 23:20:51 +0100

    Install and configure sentry

--- src/Infrastructure/Symfony/Config/packages/prod/monolog.php
+++ src/Infrastructure/Symfony/Config/packages/prod/monolog.php
@@ -5,5 +5,34 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('monolog', ['handlers' => ['main' => ['type' => 'fingers_crossed', 'action_level' => 'error', 'handler' => 'nested', 'excluded_http_codes' => [404, 405], 'buffer_size' => 50], 'nested' => ['type' => 'stream', 'path' => 'php://stderr', 'level' => 'debug', 'formatter' => 'monolog.formatter.json'], 'console' => ['type' => 'console', 'process_psr_3_messages' => false, 'channels' => ['!event', '!doctrine']]]]);
+    $containerConfigurator->extension('monolog', [
+        'handlers' => [
+            'main' => [
+                'type' => 'fingers_crossed',
+                'action_level' => 'error',
+                'handler' => 'nested',
+                'excluded_http_codes' => [404, 405],
+                'buffer_size' => 50,
+            ],
+            'nested' => [
+                'type' => 'stream',
+                'path' => 'php://stderr',
+                'level' => 'debug',
+                'formatter' => 'monolog.formatter.json',
+            ],
+            'console' => [
+                'type' => 'console',
+                'process_psr_3_messages' => false,
+                'channels' => ['!event', '!doctrine'],
+            ],
+        ],
+    ]);
 };
+
+/**
+handlers:
+sentry:
+type: sentry
+level: !php/const Monolog\Logger::ERROR
+hub_id: Sentry\State\HubInterface
+ */
