b78d44471255ba7d80a44e51ac48fb6852271c77
Jan Mikeš <j.mikes@me.com>
2022-02-02 10:12:33 +0100

    Disable CSRF protection for all forms

--- src/Infrastructure/Symfony/Config/packages/framework.php
+++ src/Infrastructure/Symfony/Config/packages/framework.php
@@ -5,5 +5,18 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('framework', ['secret' => '%env(APP_SECRET)%', 'http_method_override' => false, 'session' => ['handler_id' => null, 'cookie_secure' => 'auto', 'cookie_samesite' => 'lax', 'storage_factory_id' => 'session.storage.factory.native'], 'php_errors' => ['log' => true]]);
+    $containerConfigurator->extension('framework', [
+        'secret' => '%env(APP_SECRET)%',
+        'http_method_override' => false,
+        'csrf_protection' => false,
+        'session' => [
+            'handler_id' => null,
+            'cookie_secure' => 'auto',
+            'cookie_samesite' => 'lax',
+            'storage_factory_id' => 'session.storage.factory.native',
+        ],
+        'php_errors' => [
+            'log' => true,
+        ],
+    ]);
 };
