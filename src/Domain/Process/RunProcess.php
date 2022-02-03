4c8d292b1290aa0b14f783b7b37112ad5c2177cf
Jan Mikeš <j.mikes@me.com>
2022-02-03 17:12:07 +0100

    Shell verbosity = 0 by default for all cmds

--- src/Domain/Process/RunProcess.php
+++ src/Domain/Process/RunProcess.php
@@ -7,7 +7,6 @@ namespace Peon\Domain\Process;
 use Peon\Domain\Process\Exception\ProcessFailed;
 use Peon\Domain\Process\Value\ProcessResult;
 
-// ['SHELL_VERBOSITY' => 0]
 interface RunProcess
 {
     /**
