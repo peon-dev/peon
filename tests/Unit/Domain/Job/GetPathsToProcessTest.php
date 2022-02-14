92bb1a3d574e5238cdab1394d288ff9b9a62d9b7
Jan Mikeš <j.mikes@me.com>
2022-02-14 15:08:37 +0100

    Decouple gettings paths for process + filter files outside of psr4 roots

---
+++ tests/Unit/Domain/Job/GetPathsToProcessTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Peon\Tests\Unit\Domain\Job;
+
+use Peon\Domain\Cookbook\Value\RecipeName;
+use Peon\Domain\Job\GetPathsToProcess;
+use Peon\Domain\Job\Value\JobId;
+use Peon\Domain\Project\Value\EnabledRecipe;
+use Peon\Domain\Tools\Composer\Composer;
+use Peon\Domain\Tools\Git\Git;
+use PHPUnit\Framework\TestCase;
+
+class GetPathsToProcessTest extends TestCase
+{
+    public function testAllPSR4RootsWillBeReturned(): void
+    {
+        $git = $this->createMock(Git::class);
+
+        $composer = $this->createMock(Composer::class);
+        $composer->expects(self::once())
+            ->method('getPsr4Roots')
+            ->willReturn(['root1', 'root2']);
+
+        $getPathsToProcess = new GetPathsToProcess($git, $composer);
+
+        $paths = $getPathsToProcess->forJob(
+            new JobId(''),
+            new EnabledRecipe(RecipeName::UNUSED_PRIVATE_METHODS, null),
+            '/'
+        );
+
+        self::assertSame(['root1', 'root2'], $paths);
+    }
+
+
+    public function testOnlyChangedFilesWithinRootsWillBeReturned(): void
+    {
+        $jobId = new JobId('');
+        $workingDirectory = '/';
+
+        $git = $this->createMock(Git::class);
+        $git->expects(self::once())
+            ->method('getChangedFilesSinceCommit')
+            ->with($jobId, $workingDirectory, '12345')
+            ->willReturn(['root1/file1.php', 'root2/file2.php', 'root3/file3.php']);
+
+        $composer = $this->createMock(Composer::class);
+        $composer->expects(self::once())
+            ->method('getPsr4Roots')
+            ->willReturn(['root1', 'root2']);
+
+        $getPathsToProcess = new GetPathsToProcess($git, $composer);
+
+        $paths = $getPathsToProcess->forJob(
+            $jobId,
+            new EnabledRecipe(RecipeName::UNUSED_PRIVATE_METHODS, '12345'),
+            $workingDirectory
+        );
+
+        self::assertSame(['root1/file1.php', 'root2/file2.php'], $paths);
+    }
+}
