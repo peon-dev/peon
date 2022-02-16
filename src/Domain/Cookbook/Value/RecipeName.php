53f4d36bb29e0714b69359ae892bc9fcefcf27f9
Jan Mikeš <j.mikes@me.com>
2022-02-16 22:31:17 +0100

    Draft of new recipe: object ::class magic constant

--- src/Domain/Cookbook/Value/RecipeName.php
+++ src/Domain/Cookbook/Value/RecipeName.php
@@ -9,4 +9,5 @@ enum RecipeName: string
     case UNUSED_PRIVATE_METHODS = 'unused-private-methods';
     case TYPED_PROPERTIES = 'typed-properties';
     case SWITCH_TO_MATCH = 'switch-to-match';
+    case OBJECT_MAGIC_CLASS_CONSTANT = 'object-magic-class-constant';
 }
