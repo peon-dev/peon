e28b0ab5f3a29fc043821ddc01528e2e70c76081
Jan Mikeš <j.mikes@me.com>
2022-02-20 02:56:14 +0100

    Added save and run buttons + failing tests (you know, we are doing TDD here :-) )

--- tests/Application/Ui/Controller/RedefineTaskControllerTest.php
+++ tests/Application/Ui/Controller/RedefineTaskControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Peon\Tests\Application\Ui\Controller;
 
+use Peon\Domain\Job\JobsCollection;
 use Peon\Domain\Task\TasksCollection;
 use Peon\Domain\Task\Value\TaskId;
 use Peon\Tests\DataFixtures\DataFixtures;
@@ -51,5 +52,43 @@ STRING;
         self::assertSame('New name', $task->name);
         self::assertSame('* * * * *', $task->schedule?->getExpression());
         self::assertSame(['New command 1', 'New command 2'], $task->commands);
+    }
+
+
+
+    public function testJobWillBeScheduledUsingSaveAndRunButton(): void
+    {
+        $client = self::createClient();
+        $container = self::getContainer();
+        $tasksCollection = $container->get(TasksCollection::class);
+        $taskId = DataFixtures::TASK_ID;
+        $jobsCollection = $container->get(JobsCollection::class);
+        $jobsCountBeforeScenario = count($jobsCollection->all());
+
+        $crawler = $client->request('GET', "/redefine-task/$taskId");
+
+        $form = $crawler->selectButton('save-and-run')->form();
+
+        $commands = <<<STRING
+New command 1
+New command 2
+STRING;
+
+        $client->submit($form, [
+            $form->getName() . '[name]' => 'New name',
+            $form->getName() . '[schedule]' => '* * * * *',
+            $form->getName() . '[commands]' => $commands,
+        ]);
+
+        $jobs = $jobsCollection->all();
+        $job = $jobs[array_key_last($jobs)];
+
+        self::assertResponseRedirects("/job/" . $job->jobId->id);
+
+        $task = $tasksCollection->get(new TaskId($taskId));
+        self::assertSame('New name', $task->name);
+        self::assertSame('* * * * *', $task->schedule?->getExpression());
+        self::assertSame(['New command 1', 'New command 2'], $task->commands);
+        self::assertCount(1 + $jobsCountBeforeScenario, $jobsCollection->all());
     }
 }
