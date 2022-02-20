b0688b743b0eee22c58a69f586a2d19fb8a6589e
Jan Mikeš <j.mikes@me.com>
2022-02-20 02:56:14 +0100

    Added save and run buttons + failing tests (you know, we are doing TDD here :-) )

--- tests/Application/Ui/Controller/DefineTaskControllerTest.php
+++ tests/Application/Ui/Controller/DefineTaskControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Peon\Tests\Application\Ui\Controller;
 
+use Peon\Domain\Job\JobsCollection;
 use Peon\Domain\Task\TasksCollection;
 use Peon\Tests\DataFixtures\DataFixtures;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -44,5 +45,37 @@ final class DefineTaskControllerTest extends WebTestCase
         self::assertResponseRedirects("/projects/$projectId");
 
         self::assertCount(1 + $tasksCountBeforeScenario, $tasksCollection->all());
+    }
+
+
+    public function testJobWillBeScheduledUsingSaveAndRunButton(): void
+    {
+        $client = self::createClient();
+        $container = self::getContainer();
+        $tasksCollection = $container->get(TasksCollection::class);
+        $tasksCountBeforeScenario = count($tasksCollection->all());
+        $jobsCollection = $container->get(JobsCollection::class);
+        $jobsCountBeforeScenario = count($jobsCollection->all());
+
+        $projectId = DataFixtures::PROJECT_1_ID;
+        $crawler = $client->request('GET', "/define-task/$projectId");
+
+        self::assertResponseIsSuccessful();
+
+        $form = $crawler->selectButton('save-and-run')->form();
+
+        $client->submit($form, [
+            $form->getName() . '[name]' => 'Test',
+            $form->getName() . '[schedule]' => '* * * * *',
+            $form->getName() . '[commands]' => 'command',
+        ]);
+
+        $jobs = $jobsCollection->all();
+        $job = $jobs[array_key_last($jobs)];
+
+        self::assertResponseRedirects("/job/" . $job->jobId->id);
+
+        self::assertCount(1 + $tasksCountBeforeScenario, $tasksCollection->all());
+        self::assertCount(1 + $jobsCountBeforeScenario, $jobsCollection->all());
     }
 }
