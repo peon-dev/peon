bcf31d5a17a21e45d9bfa008dc5063aaa2b7438d
Jan Mikeš <j.mikes@me.com>
2022-02-24 00:53:04 +0100

    Use Europe/Prague timezone in twig

--- src/Infrastructure/Symfony/Config/packages/twig.php
+++ src/Infrastructure/Symfony/Config/packages/twig.php
@@ -8,5 +8,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('twig', [
         'default_path' => '%kernel.project_dir%/src/Ui/templates',
         'form_themes' => ['bootstrap_5_layout.html.twig'],
+        'date' => [
+            'timezone' => 'Europe/Prague',
+        ],
     ]);
 };
