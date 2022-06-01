90c630b3a460854e94ae4526d5a3be97a656f936
Jan Mikeš <j.mikes@me.com>
2022-06-01 19:50:50 +0200

    Security config in php

--- src/Infrastructure/Symfony/Config/packages/security.php
+++ src/Infrastructure/Symfony/Config/packages/security.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Config\SecurityConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('security', ['enable_authenticator_manager' => true, 'password_hashers' => [PasswordAuthenticatedUserInterface::class => 'auto'], 'providers' => ['users_in_memory' => ['memory' => null]], 'firewalls' => ['dev' => ['pattern' => '^/(_(profiler|wdt)|css|images|js)/', 'security' => false], 'main' => ['lazy' => true, 'provider' => 'users_in_memory']], 'access_control' => null]);
+return static function (ContainerConfigurator $containerConfigurator, SecurityConfig $securityConfig): void {
+    $securityConfig->enableAuthenticatorManager(true);
+
+    $securityConfig->passwordHasher(PasswordAuthenticatedUserInterface::class, 'auto');
+
+    $securityConfig->provider('users_in_memory')
+        ->memory([]);
+
+    $securityConfig->firewall('dev')
+        ->pattern('^/(_(profiler|wdt)|css|images|js)/')
+        ->security(false);
+
+    $securityConfig->firewall('main')
+        ->lazy(true)
+        ->provider('users_in_memory');
 };
