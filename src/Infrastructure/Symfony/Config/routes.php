2eb291427ba51ac3070cb6378f09f664dc9f9f21
Jan Mikeš <j.mikes@me.com>
2022-06-22 11:06:54 +0200

    Implemented logout feature

--- src/Infrastructure/Symfony/Config/routes.php
+++ src/Infrastructure/Symfony/Config/routes.php
@@ -6,4 +6,7 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
     $routingConfigurator->import(__DIR__ . '/../../../Ui/Controller/', 'annotation');
+
+    $routingConfigurator->add('logout', '/logout')
+        ->methods(['GET']);
 };
