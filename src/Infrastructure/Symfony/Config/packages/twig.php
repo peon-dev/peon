e4e65c39528d1dc68ab244082c89662682f21458
Jan Mikeš <j.mikes@me.com>
2022-06-25 01:37:42 +0200

    Remove footer navbar

--- src/Infrastructure/Symfony/Config/packages/twig.php
+++ src/Infrastructure/Symfony/Config/packages/twig.php
@@ -2,20 +2,14 @@
 
 declare(strict_types=1);
 
-use Composer\InstalledVersions;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $composerPackage = InstalledVersions::getRootPackage();
-
     $containerConfigurator->extension('twig', [
         'default_path' => '%kernel.project_dir%/src/Ui/templates',
         'form_themes' => ['bootstrap_5_layout.html.twig'],
         'date' => [
             'timezone' => 'Europe/Prague',
-        ],
-        'globals' => [
-            'peon_app_version' => $composerPackage['pretty_version'],
         ],
     ]);
 };
