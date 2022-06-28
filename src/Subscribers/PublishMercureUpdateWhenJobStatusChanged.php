ef73bc28827f067463a579e1ac0d0356eebfdea1
Jan Mikeš <j.mikes@me.com>
2022-06-28 22:34:53 +0200

    Prevent mercure from shutting down application when not available

--- src/Subscribers/PublishMercureUpdateWhenJobStatusChanged.php
+++ src/Subscribers/PublishMercureUpdateWhenJobStatusChanged.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Peon\Subscribers;
 
 use Peon\Domain\Job\Event\JobStatusChanged;
-use Peon\Domain\Job\Exception\JobNotFound;
 use Peon\Packages\MessageBus\Event\EventHandlerInterface;
 use Peon\Ui\ReadModel\Job\ProvideReadJobById;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Twig\Environment;
@@ -15,35 +15,39 @@ use Twig\Environment;
 final class PublishMercureUpdateWhenJobStatusChanged implements EventHandlerInterface
 {
     public function __construct(
-        private HubInterface $hub,
-        private Environment $twig,
-        private ProvideReadJobById $provideReadJobById,
+        private readonly HubInterface $hub,
+        private readonly Environment $twig,
+        private readonly ProvideReadJobById $provideReadJobById,
+        private readonly LoggerInterface $logger,
     ) {}
 
 
-    /**
-     * @throws JobNotFound
-     */
     public function __invoke(JobStatusChanged $event): void
     {
-        $job = $this->provideReadJobById->provide($event->jobId);
+        try {
+            $job = $this->provideReadJobById->provide($event->jobId);
 
-        $this->hub->publish(
-            new Update(
-                'project-' . $event->projectId->id . '-overview',
-                $this->twig->render('job_status_changed.stream.html.twig', [
-                    'job' => $job,
-                ])
-            )
-        );
+            $this->hub->publish(
+                new Update(
+                    'project-' . $event->projectId->id . '-overview',
+                    $this->twig->render('job_status_changed.stream.html.twig', [
+                        'job' => $job,
+                    ])
+                )
+            );
 
-        $this->hub->publish(
-            new Update(
-                'dashboard',
-                $this->twig->render('dashboard.job_status_changed.stream.html.twig', [
-                    'job' => $job,
-                ])
-            )
-        );
+            $this->hub->publish(
+                new Update(
+                    'dashboard',
+                    $this->twig->render('dashboard.job_status_changed.stream.html.twig', [
+                        'job' => $job,
+                    ])
+                )
+            );
+        } catch (\Throwable $throwable) {
+            $this->logger->warning($throwable->getMessage(), [
+                'exception' => $throwable,
+            ]);
+        }
     }
 }
--- src/Subscribers/PublishMercureUpdateWhenRecipeEnabled.php
+++ src/Subscribers/PublishMercureUpdateWhenRecipeEnabled.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Peon\Subscribers;
 
 use Peon\Domain\Cookbook\Event\RecipeEnabled;
-use Peon\Domain\Project\Exception\ProjectNotFound;
 use Peon\Packages\MessageBus\Event\EventHandlerInterface;
 use Peon\Ui\ReadModel\Dashboard\ProvideReadProjectById;
 use Peon\Ui\ReadModel\ProjectDetail\ProvideProjectReadRecipes;
 use Peon\Ui\ReadModel\ProjectDetail\ProvideReadProjectDetail;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
 use Twig\Environment;
@@ -17,49 +17,53 @@ use Twig\Environment;
 final class PublishMercureUpdateWhenRecipeEnabled implements EventHandlerInterface
 {
     public function __construct(
-        private HubInterface              $hub,
-        private Environment               $twig,
-        private ProvideProjectReadRecipes $provideProjectReadRecipes,
-        private ProvideReadProjectDetail  $provideReadProjectDetail,
-        private ProvideReadProjectById $provideReadProjectById,
+        private readonly HubInterface              $hub,
+        private readonly Environment               $twig,
+        private readonly ProvideProjectReadRecipes $provideProjectReadRecipes,
+        private readonly ProvideReadProjectDetail  $provideReadProjectDetail,
+        private readonly ProvideReadProjectById $provideReadProjectById,
+        private readonly LoggerInterface $logger,
     ) {}
 
 
-    /**
-     * @throws ProjectNotFound
-     */
     public function __invoke(RecipeEnabled $event): void
     {
-        $projectDetail = $this->provideReadProjectDetail->provide($event->projectId);
-        $project = $this->provideReadProjectById->provide($event->projectId);
+        try {
+            $projectDetail = $this->provideReadProjectDetail->provide($event->projectId);
+            $project = $this->provideReadProjectById->provide($event->projectId);
 
-        $this->hub->publish(
-            new Update(
-                'project-' . $event->projectId->id . '-cookbook',
-                $this->twig->render('cookbook.stream.html.twig', [
-                    'project' => $projectDetail,
-                    'recipeName' => $event->recipeName
-                ])
-            )
-        );
+            $this->hub->publish(
+                new Update(
+                    'project-' . $event->projectId->id . '-cookbook',
+                    $this->twig->render('cookbook.stream.html.twig', [
+                        'project' => $projectDetail,
+                        'recipeName' => $event->recipeName
+                    ])
+                )
+            );
 
-        $this->hub->publish(
-            new Update(
-                'project-' . $event->projectId->id . '-overview',
-                $this->twig->render('recipes.stream.html.twig', [
-                    'project' => $projectDetail,
-                    'recipes' => $this->provideProjectReadRecipes->provide($event->projectId),
-                ])
-            )
-        );
+            $this->hub->publish(
+                new Update(
+                    'project-' . $event->projectId->id . '-overview',
+                    $this->twig->render('recipes.stream.html.twig', [
+                        'project' => $projectDetail,
+                        'recipes' => $this->provideProjectReadRecipes->provide($event->projectId),
+                    ])
+                )
+            );
 
-        $this->hub->publish(
-            new Update(
-                'dashboard',
-                $this->twig->render('dashboard.project_stats.stream.html.twig', [
-                    'project' => $project,
-                ])
-            )
-        );
+            $this->hub->publish(
+                new Update(
+                    'dashboard',
+                    $this->twig->render('dashboard.project_stats.stream.html.twig', [
+                        'project' => $project,
+                    ])
+                )
+            );
+        } catch (\Throwable $throwable) {
+            $this->logger->warning($throwable->getMessage(), [
+                'exception' => $throwable,
+            ]);
+        }
     }
 }
